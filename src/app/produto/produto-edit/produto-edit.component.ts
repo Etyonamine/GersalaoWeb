247172d260276d0ba06a22ed50cf4526c2850a69
Eduardo Toshio Yonamine <60787694+Etyonamine@users.noreply.github.com>
2021-11-23 22:44:49 -0300

    criacao da rotina de recuperar tipo e linha

--- src/app/produto/produto-edit/produto-edit.component.ts
+++ src/app/produto/produto-edit/produto-edit.component.ts
@@ -1,35 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { removeSummaryDuplicates } from '@angular/compiler';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
 import { ProdutoLinha } from 'src/app/produto-linha/produto-linha';
+import { ProdutoLinhaService } from 'src/app/produto-linha/produto-linha.service';
 import { AlertService } from 'src/app/shared/alert/alert.service';
 import { BaseFormComponent } from 'src/app/shared/base-form/base-form.component';
+import { ApiResult } from 'src/app/shared/base.service';
 import { TipoProduto } from 'src/app/tipo-produto/tipo-produto';
-import { Produto } from '../produto';
+import { TipoProdutoService } from 'src/app/tipo-produto/tipo-produto.service';
 
 @Component({
   selector: 'app-produto-edit',
   templateUrl: './produto-edit.component.html',
   styleUrls: ['./produto-edit.component.scss']
 })
-export class ProdutoEditComponent extends BaseFormComponent implements OnInit {
+export class ProdutoEditComponent extends BaseFormComponent implements OnInit, OnDestroy {
   submit() {
     throw new Error('Method not implemented.');
   }
   formulario: FormGroup;
   tituloPagina: string;
   codigo: number;
-  tipos: Array<TipoProduto>=[];
+  tipos: Array<TipoProduto> =[];
   linhas: Array<ProdutoLinha>=[];
+
+  inscricaoTipo$: Subscription;
+  inscricaoLinhas$: Subscription;
 
   constructor(
       private formBuilder:FormBuilder,
       private alertService: AlertService,
+      private tipoProdutoService: TipoProdutoService,
+      private linhaProdutoService:ProdutoLinhaService,
       ) {
     super();
    }  
   ngOnInit(): void {
     this.criarFormulario();
+    this.carregarTipos();
+    this.carregarLinhas();
+  }
+  ngOnDestroy(): void{
+    if(this.inscricaoTipo$){
+      this.inscricaoTipo$.unsubscribe();
+    }
+    if(this.inscricaoLinhas$){
+      this.inscricaoLinhas$.unsubscribe();
+    }
   }
   criarFormulario(){
     this.formulario = this.formBuilder.group ({
@@ -38,15 +57,58 @@ export class ProdutoEditComponent extends BaseFormComponent implements OnInit {
       observacao: [null], 
       tipo: [0,[Validators.required]],
       linha:[0,[Validators.required]],
+      codigoFornecedor:[null],
       situacao : [1,[Validators.required]]
     })
   }
- 
-
+  carregarTipos(){
+    this.inscricaoTipo$ = this.tipoProdutoService.getData<ApiResult<TipoProduto>>(
+      0,
+      100,
+      "nome",
+      "asc",
+      null,
+      null
+    ).subscribe(result=>{
+        result.data.forEach(tipo=>{
+          this.tipos.push(tipo);
+        });        
+            
+    },error=>{
+      console.log(error);
+      this.handleError('Ocorreu erro ao listar os tipos');
+    });
+  }
+  carregarLinhas(){
+    this.inscricaoLinhas$ = this.linhaProdutoService.getData<ApiResult<ProdutoLinha>>(
+      0,
+      100,
+      "nome",
+      "asc",
+      null,
+      null
+    ).subscribe(result=>{
+        result.data.forEach(linha=>{
+          this.linhas.push(linha);
+        });        
+            
+    },error=>{
+      console.log(error);
+      this.handleError('Ocorreu erro ao listar as linhas de produto');
+    });
+  }
   openConfirmExclusao(){
 
   }
   isDupe(){
 
+  }
+  
+  handlerSuccess(msg: string) {
+    this.alertService.mensagemSucesso(msg);
+  }
+
+  handleError(msg: string) {
+    this.alertService.mensagemErro(msg);
   }
 }
