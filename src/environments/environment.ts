4c9b857202c16dac5bbc5787785276e2610734bc
Eduardo Toshio Yonamine <60787694+Etyonamine@users.noreply.github.com>
2022-03-13 19:18:05 -0300

    alteracao-apontamento-ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API: 'https://localhost:44368/api/'
+  production: true,
+  API: 'http://192.168.0.119:40/api/'
+ /*  API: 'http://192.168.0.86:30/api/' */
 
 };
 
