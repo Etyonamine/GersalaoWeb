f6e40f00a6824f0e35ddab890d04e106c92212ec
ETYonamine <etyonamine@users.noreply.github.com>
2022-12-06 22:31:00 -0300

    alteracao na classe empresa

--- src/app/empresa/empresa.ts
+++ src/app/empresa/empresa.ts
@@ -1,14 +1,12 @@
-import { Time } from "@angular/common";
-import * as internal from "stream";
 import { EmpresaContato } from "./empresa-contato";
 import { EmpresaEndereco } from "./empresa-endereco";
-
 export interface Empresa{
     codigo: string;
     nome : string;
     nomeAbreviado : string;
     horaInicial: string;
     horaFim: string;
+    logo: string;
     quantidadeMinutosServico: string;
     empresaContato:EmpresaContato;
     empresaEndereco:EmpresaEndereco;
