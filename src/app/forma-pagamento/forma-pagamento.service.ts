08222c4b5739844bea98fa50b58551aca1b1e7d5
ETYonamine <etyonamine@users.noreply.github.com>
2023-02-04 19:12:34 -0300

    correção da lista de formas de pagto da apr

--- src/app/forma-pagamento/forma-pagamento.service.ts
+++ src/app/forma-pagamento/forma-pagamento.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { BaseService } from '../shared/base.service';
 import { FormaPagamento } from './forma-pagamento';
@@ -8,9 +9,14 @@ import { FormaPagamento } from './forma-pagamento';
   providedIn: 'root'
 })
 export class FormaPagamentoService extends BaseService<FormaPagamento>{
+  url: string = `${environment.API}FormasPagamento`;
 
   constructor(protected http: HttpClient)
   {
     super(http, `${environment.API}FormasPagamento`);
+   }
+   listarFormasPagamentoApuracao(){
+    let urlListaApr = this.url +  '/ListaFormaPagtoApuracao';
+    return this.http.get<FormaPagamento[]>(urlListaApr).pipe(take(1));
    }
 }
