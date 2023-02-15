2de03431135bd29ce77d07c8297a244fb57072c4
ETYonamine <etyonamine@users.noreply.github.com>
2023-02-15 19:38:43 -0300

    ajustando o caixa detalhe

--- src/app/caixa/caixa-fechar/caixa-fechar.component.ts
+++ src/app/caixa/caixa-fechar/caixa-fechar.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AlertService } from 'src/app/shared/alert/alert.service';
 import { BaseFormComponent } from 'src/app/shared/base-form/base-form.component';
 import { Caixa } from '../caixa';
+import { CaixaDetalhe } from '../caixa-detalhe';
+import { CaixaDetalheService } from '../caixaDetalhe.service';
 
 @Component({
   selector: 'app-caixa-fechar',
@@ -13,18 +19,42 @@ import { Caixa } from '../caixa';
 export class CaixaFecharComponent extends BaseFormComponent implements OnInit {
  
   caixa: Caixa;
+  listaDetalhes: MatTableDataSource<CaixaDetalhe>;
+  defaultFilterColumn: string = "dataCadastro";
+  filterQuery: string = null;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;  
+  colunas: string[] = [ "codigo", "dataAbertura", "dataFechamento", "valorInicial", "valorFinal" ,"acao"];
+  defaultPageIndex: number = 0;
+  defaultPageSize: number = 10;
+  public defaultSortColumn: string = "dataAbertura";
+  public defaultSortOrder: string = "desc";
+  
   formulario: FormGroup;
+
+  inscricao$: Subscription;
+  codigoCaixa: number;
 
   constructor(private route: ActivatedRoute,
               private formBuilder: FormBuilder,
               private alertService: AlertService,
-              private router:Router) {
+              private router:Router,
+              private caixaDetalheService: CaixaDetalheService) {
     super();
   }
   ngOnInit(): void {
     this.caixa = this.route.snapshot.data['caixa'];    
+    this.codigoCaixa = this.caixa.codigo;
     this.criacaoFormulario();
+    this.recuperarListaDetalhe();
   }  
+  recuperarListaDetalhe(){
+    this.inscricao$ = this.caixaDetalheService.recuperarLista(this.codigoCaixa)
+                                              .subscribe(result=>{
+                                                this.listaDetalhes = result;
+                                              });
+
+  }
   criacaoFormulario() {
     let codigoParam = 0;    
     let observcaoParam : string = '';
